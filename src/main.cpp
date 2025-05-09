0055861ae0fefd80767475ffbb380925f93d3e80
LEE HYUN SU <phantom7547@gmail.com>
2025-05-09 03:35:48 +0000

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include "person.h"
 
 int main() {
-    Person p("Alice", 30);
+    Person p("HyunSu", 30);
     std::cout << p.getName() << " is " << p.getAge() << " years old." << std::endl;
     return 0;
+
+
 }
