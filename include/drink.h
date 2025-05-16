eedef20bac25851eddc4bdc9baaf8a3879cb12cd
Baikhyunjun <efc3825@konkuk.ac.kr>
2025-05-16 23:06:14 +0900

    add drink.h

---
+++ include/drink.h
@@ -0,0 +1,21 @@
+#ifndef DRINK_H
+#define DRINK_H
+
+#include <string>
+
+
+class Drink {
+    public:
+        Drink(std::string name, double price, std::string code);
+
+        std::string getName() const;
+        int getPrice() const;
+        std::string getCode() const;
+
+    private:
+        std:: string  name_;
+        int price_;
+        std:: string code_;
+};
+
+#endif
