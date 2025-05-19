04601e9022f04232c5760cb2f8b4135365787a1c
jinsso_ol <jinsol982@gmail.com>
2025-05-19 20:47:33 +0000

    bebug2

--- include/service/UserProcessController.hpp
+++ include/service/UserProcessController.hpp
@@ -18,7 +18,7 @@ public:
     void handleMenu();
     void handlePrepayCode();
     void handleDrinkSelection();
-    void UserProcessController::handlePayment();
+    void handlePayment(/*const bool& isPrepay*/);
     void nofityError(const std::string& error);
     void nearestVM(const network::Message& msg);
     void showPrepaymentCode(const std::string& prepaymentCode);
