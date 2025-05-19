3e2a77037e70915f1a06ef31089a3fcf4fb2b09c
Baikhyunjun <efc3825@konkuk.ac.kr>
2025-05-19 13:44:47 +0000

    uc1 getlist Success

--- src/persistence/inventoryRepository.cpp
+++ src/persistence/inventoryRepository.cpp
@@ -4,7 +4,28 @@
 using namespace domain;
 
 namespace persistence {
-std::vector<domain::inventory> inventoryRepository::allDrinks;
+std::vector<domain::inventory> inventoryRepository::allDrinks = {
+    inventory(Drink("펩시", 1500, "D001"), 10),
+    inventory(Drink("스프라이트", 1500, "D002"), 10),
+    inventory(Drink("비타500", 1600, "D003"), 10),
+    inventory(Drink("파워에이드", 1600, "D004"), 10),
+    inventory(Drink("몬스터", 2500, "D005"), 10),
+    inventory(Drink("오렌지주스", 2700, "D006"), 10),
+    inventory(Drink("게토레이", 1800, "D007"), 10),
+    inventory(Drink("옥수수 수염차", 1800, "D008"), 10),
+    inventory(Drink("박카스", 1700, "D009"), 10),
+    inventory(Drink("비타민음료", 1000, "D010"), 10),
+    inventory(Drink("보성녹차", 1500, "D011"), 10),
+    inventory(Drink("밀키스", 1500, "D012"), 10),
+    inventory(Drink("코코팜", 1600, "D013"), 10),
+    inventory(Drink("트로피카나", 1600, "D014"), 10),
+    inventory(Drink("카페라떼 캔커피", 1200, "D015"), 10),
+    inventory(Drink("아메리카노 캔커피", 1200, "D016"), 10),
+    inventory(Drink("레쓰비", 1300, "D017"), 10),
+    inventory(Drink("포카리스웨트", 1400, "D018"), 10),
+    inventory(Drink("환타 오렌지", 1500, "D019"), 10),
+    inventory(Drink("칸타타", 1600, "D020"), 10)
+};
 
 void inventoryRepository::setAllDrinks(const std::vector<domain::inventory>& drinks) {
     allDrinks = drinks;
--- src/service/InventoryService.cpp
+++ src/service/InventoryService.cpp
@@ -1,22 +1,24 @@
 // InventoryService.cpp
 #include "service/InventoryService.hpp"
+#include "../include/persistence/inventoryRepository.h"
 #include <vector>
 #include <string>
 namespace service {
 std::vector<std::pair<std::string, int>> InventoryService::CallInventorySer() {
-    return {};  // ?ž„?‹œ ë°˜í™˜
+    persistence::inventoryRepository repo;
+    return repo.getList();
 }
 
 bool InventoryService::getSaleValid(const std::string& drink) {
-    return true;  // ?ž„?‹œ ë°˜í™˜
+    return true;  // ?? ë°˜í™˜
 }
 
 std::string InventoryService::reduceDrink(const std::string& drink) {
-    return drink;  // ?ž„?‹œ ë°˜í™˜
+    return drink;  // ?ï¿½ï¿½?ï¿½ï¿½ ë°˜í™˜
 }
 
 bool InventoryService::validOVMStock(const std::string& drinkName) {
-    return true;  // ?ž„?‹œ ë°˜í™˜
+    return true;  // ?ï¿½ï¿½?ï¿½ï¿½ ë°˜í™˜
 }
 
 
